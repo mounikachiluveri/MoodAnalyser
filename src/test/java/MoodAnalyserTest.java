3f347bb8ef5d499ab7cfbca2971534e2448f7ea5
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-23 22:17:13 +0530

    [Mounika] Added:Checking the sad mood return the sad

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -5,14 +5,8 @@ public class MoodAnalyserTest {
     @Test
     public void testMoodAnalyserSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is sad message");
+        String mood = moodAnalyser.analyseMood("Iam in sad mood");
         Assert.assertEquals("sad", mood);
     }
 
-    @Test
-    public void testMoodAnalyserHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is Happy Message");
-        Assert.assertEquals("happy", mood);
-    }
 }
