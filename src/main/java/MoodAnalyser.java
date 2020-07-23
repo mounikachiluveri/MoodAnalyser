3b99101746f8e2383a3b4254820e5476729e7ec4
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-23 22:43:45 +0530

    [Mounika] Refactor:code Refactord and create constructer

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,4 +1,13 @@
 public class MoodAnalyser {
+    String message;
+
+    public MoodAnalyser() {
+        message="default";
+    }
+    void MoodAnalyser(String message)
+    {
+        this.message=message;
+    }
     public String analyseMood(String message) {
         return "Happy";
     }
